52bcc3532e1b531493c5a42b02e7695342416d2e
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2024-03-22 15:49:05 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -0,0 +1,9 @@
+-- Create States Table In hbtn_0e_0_usa With Some Data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
